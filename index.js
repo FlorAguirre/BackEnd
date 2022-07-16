3392c11a5030dd142ccbe4a57161acea80f7f619
FlorAguirre <floraguirre1598@gmail.com>
2022-07-15 22:04:08 -0400

    Desafio 1

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
-//Terminos
-// undefined puede salir porque declaras una variable como undefined, porque no asignaste valor a una variable
-// null 
+//Desafio 1
 
-let myArray = [1,2,3]
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas){
+    this. nombre = nombre;
+    this.apellido = apellido;
+    this.libros = libros;
+    this.mascotas = mascotas;
+    this.librosADevolver = [];
+    }
 
-console.log('bienvenidos');
-console.log(myArray)
+    getFullName() {
+        return console.log(`El usuario se llama ${this.nombre} y se apellida ${this.apellido}`);
+    }
+        
+    addMascotas(nombreMascota) {
+        this.mascotas.push(nombreMascota);
 
+    }
+    countMascotas() {
+        return console.log(`La cantidad de mascotas es ${this.mascotas.length}`);
+    }
+
+    addBook(libro){
+        this.libros.push(libro);
+    }
+
+    getBookNames(){
+        this.libros.forEach(libro => {
+            this.librosADevolver.push(libro.nombre);
+            
+        });
+
+        console.log(this.librosADevolver)
+
+    }
+}
+
+let usuarioP = new Usuario(
+    'Flor',
+    'Aguirre',
+
+    [
+        {nombre: 'Romeo y Julieta' , Autor: 'William Shakespeare' },
+        {nombre: 'Orgullo y Prejuicio' ,  Autor: 'Jane Austen'},
+    ],
+    ['Perro' , 'Gato']
+);
+
+usuarioP.getFullName();
+usuarioP.addMascotas('Tortuga');
+usuarioP.countMascotas();
+usuarioP.addBook({nombre: 'Dracula', Autor: 'Bram Stoker'});
+usuarioP.getBookNames();
