d96abbd6795b212be616a2a94cb2bc6f3588010b
FlorAguirre <floraguirre1598@gmail.com>
2022-07-15 21:29:59 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//Terminos
+// undefined puede salir porque declaras una variable como undefined, porque no asignaste valor a una variable
+// null 
+
+let myArray = [1,2,3]
+
+console.log('bienvenidos');
+console.log(myArray)
+
